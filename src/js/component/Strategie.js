fba42986cf6faff7ad43f9ed2e815ced783e0677
jgustavoj <gjime011@fiu.edu>
2020-09-19 01:52:28 +0000

    strategie

---
+++ src/js/component/Strategie.js
@@ -0,0 +1,6 @@
+//1. Create an input field to collect task from the user
+//2. Create array of objects to store task 
+//3. Device a way to store the value of the input field into the array (function)
+//4. Add an event listener to our input field - onChange() will need to call a function 
+//5. Create a variable to store the value of input field
+//6. The function from onChange event needs to set the hook variable with the current value of input 
